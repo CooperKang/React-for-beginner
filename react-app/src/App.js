69695efa2aa124106de57872e0329abd6c281376
JaeHaKang <jhkang3457@naver.com>
2021-12-21 18:23:48 +0900

    니꼬 #7.2 (coin tracker)

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,35 +1,50 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
-  const [toDo, setToDo] = useState('');
-  const [toDos, setToDos] = useState([]);
-  const onChange = (event) => setToDo(event.target.value);
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if (toDo === '') {
-      return;
-    }
-    setToDos((currentArray) => [toDo, ...currentArray]);
-    setToDo('');
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  const [cost, setCost] = useState(1);
+  const [need, setNeed] = useState(1);
+  const onChange = (event) => {
+    setCost(event.target.value);
+    setNeed(1);
   };
+  const handleInput = (event) => {
+    setNeed(event.target.value);
+  };
+  useEffect(() => {
+    fetch('https://api.coinpaprika.com/v1/tickers')
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
+  }, []);
   return (
     <div>
-      <h1>My To Dos({toDos.length})</h1>
-      <form onSubmit={onSubmit}>
-        <input
-          onChange={onChange}
-          value={toDo}
-          type="text"
-          placeholder="Write your toDo"
-        />
-        <button>Add To Do</button>
-      </form>
-      <hr />
-      <ul>
-        {toDos.map((item, index) => (
-          <li key={index}>{item}</li>
-        ))}
-      </ul>
+      <h1>The coins!</h1>
+      <em>({coins.length}개의 코인을 찾았습니다.)</em>
+      {loading ? (
+        <strong>loading...</strong>
+      ) : (
+        <div>
+          코인을 선택하세요:&nbsp;
+          <input type="text" list="coinList" onChange={onChange} />
+          <datalist id="coinList">
+            <option>Select Coin!</option>
+            {coins.map((coin) => (
+              <option key={coin.id} value={coin.quotes.USD.price}>
+                {coin.name}({coin.symbol}) : ${coin.quotes.USD.price} USD
+              </option>
+            ))}
+          </datalist>
+        </div>
+      )}
+      <h2>넣을 돈을 입력하시오</h2>
+      <div>
+        <input type="number" value={need} onChange={handleInput} />$
+      </div>
+      <h2>You can get {need / cost} BTC</h2>
     </div>
   );
 }
